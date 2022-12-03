8fdeae7b745177de0dc1885f521534d0fe984c3c
tejasakbari <119645699+tejasakbari@users.noreply.github.com>
2022-12-03 10:13:10 +0530

    Add files via upload

---
+++ Module1/DatatypeEx.java
@@ -0,0 +1,22 @@
+
+public class DatatypeEx {
+	
+	 public static void main(String[] args)
+	 
+	 {
+		 int number = 20;
+		 
+		 int number2 = 40;
+		 
+		 int add = number2 + number;
+		 int sub = number2 - number;
+		 int mul = number2 * number;
+		 int div = number2 / number;
+		 
+		 System.out.println(add);
+		 System.out.println(sub);
+		 System.out.println(mul);
+		 System.out.println(div);
+	 }
+
+}
