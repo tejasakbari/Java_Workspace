789cd21ad386163524cdfc2c7ce59f5b0fce6fd9
tejasakbari <119645699+tejasakbari@users.noreply.github.com>
2022-12-20 11:58:42 +0530

    Add files via upload

---
+++ Assignment/Bank.java
@@ -0,0 +1,1 @@
+package Module3.java;
---
+++ Assignment/complexnumber.java
@@ -0,0 +1,97 @@
+package Module3.java;
+import java.util.*;
+
+
+class Complex {
+
+ 
+ int real, imaginary;
+
+ 
+ Complex()
+ {
+ }
+
+
+ Complex(int tempReal, int tempImaginary)
+ {
+     real = tempReal;
+     imaginary = tempImaginary;
+ }
+
+ Complex addComp(Complex C1, Complex C2)
+ {
+   
+     Complex temp = new Complex();
+
+    
+     temp.real = C1.real + C2.real;
+
+   
+     temp.imaginary = C1.imaginary + C2.imaginary;
+
+    
+     return temp;
+ }
+
+
+ Complex subtractComp(Complex C1, Complex C2)
+ {
+     
+     Complex temp = new Complex();
+
+    
+     temp.real = C1.real - C2.real;
+
+     
+     temp.imaginary = C1.imaginary - C2.imaginary;
+
+    
+     return temp;
+ }
+
+ 
+ void printComplexNumber()
+ {
+     System.out.println("Complex number: "
+                        + real + " + "
+                        + imaginary + "i");
+ }
+}
+
+
+public class complexnumber
+{
+
+    
+    public static void main(String[] args)
+    {
+ 
+        
+        Complex C1 = new Complex(3, 2);
+ 
+       
+        C1.printComplexNumber();
+ 
+      
+        Complex C2 = new Complex(9, 5);
+ 
+        C2.printComplexNumber();
+ 
+        Complex C3 = new Complex();
+ 
+       
+        C3 = C3.addComp(C1, C2);
+ 
+        
+        System.out.print("Sum of ");
+        C3.printComplexNumber();
+ 
+        
+        C3 = C3.subtractComp(C1, C2);
+ 
+        
+        System.out.print("Difference of ");
+        C3.printComplexNumber();
+    }
+}
